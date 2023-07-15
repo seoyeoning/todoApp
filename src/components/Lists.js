f54c2f6055bc26d656f82f6a08dad999df7f2926
seoyeoning <minalunpark@naver.com>
2023-07-16 00:18:23 +0900

    List 컴포넌트 분리

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,9 +1,10 @@
 // 할 일 목록 나타나는 리스트 컴포넌트로 분리
-// rcf : 함수형 컴포넌트
+// rcf(export 포함) : 함수형 컴포넌트 , rfce(export 따로)
 // rce : 클래스형 컴포넌트
 
 import React from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import List from "./List";
 
 export default function Lists({ todoData, setTodoData }) {
   // x버튼 (함수형 만들 때 const 추가해주기)
@@ -15,26 +16,6 @@ export default function Lists({ todoData, setTodoData }) {
   //   cursor: "pointer",
   //   float: "right",
   // };
-
-  // checkbox 바꿔주는 함수
-  const handleCompleChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    //this.setState({ todoData: newTodoData }); 클래스
-    setTodoData(newTodoData);
-  };
-
-  // 할 일 목록 삭제할 때
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id);
-    //console.log('newTodoData', newTodoData)
-    // this.setState({ todoData: newTodoData }); 클래스 컴포넌트
-    setTodoData(newTodoData);
-  };
 
   // getStyle 함수 (체크박스 누르면 줄 긋도록 동적으로 만들어야함)
   // const getStyle = (completed) => {
@@ -78,38 +59,16 @@ export default function Lists({ todoData, setTodoData }) {
                   index={index}
                 >
                   {(provided, snapshot) => (
-                    <div
+                    <List
                       key={data.id}
-                      {...provided.draggableProps}
-                      ref={provided.innerRef}
-                      {...provided.dragHandleProps}
-                      className={`${
-                        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                      } flex items-center justify-between h-full px-4 py-1 my-2 text-gray-600 border rounded`}
-                    >
-                      <div className="items-center">
-                        <input
-                          type="checkbox"
-                          defaultChecked={data.completed}
-                          onChange={() => handleCompleChange(data.id)}
-                        />{" "}
-                        <span
-                          className={
-                            data.completed ? "line-through" : undefined
-                          }
-                        >
-                          {data.title}{" "}
-                        </span>
-                      </div>
-                      <div className="items-center">
-                        <button
-                          className="px-4 py-2"
-                          onClick={() => handleClick(data.id)}
-                        >
-                          X
-                        </button>
-                      </div>
-                    </div>
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
                   )}
                 </Draggable>
               ))}
