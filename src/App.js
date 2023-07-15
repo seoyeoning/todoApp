3a3f509811b27b6b8a5c473a6863d061d5582cc6
seoyeoning <minalunpark@naver.com>
2023-07-15 20:10:25 +0900

    tailwind css 적용

--- src/App.js
+++ src/App.js
@@ -34,13 +34,11 @@ export default function App() {
 
   // 함수형은 render 없이 바로 return
   return (
-    <div className="container">
-      <div className="todoBlock">
-        <div className="title">
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
         </div>
-
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
         {/* props로 내려줌 (자식 컴포넌 트에) */}
         <List todoData={todoData} setTodoData={setTodoData} />
--- src/components/List.js
+++ src/components/List.js
@@ -6,14 +6,14 @@ import React from "react";
 
 export default function List({ todoData, setTodoData }) {
   // x버튼 (함수형 만들 때 const 추가해주기)
-  const btnstyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right",
-  };
+  // const btnstyle = {
+  //   color: "#fff",
+  //   border: "none",
+  //   padding: "5px 9px",
+  //   borderRadius: "50%",
+  //   cursor: "pointer",
+  //   float: "right",
+  // };
 
   // checkbox 바꿔주는 함수
   const handleCompleChange = (id) => {
@@ -36,27 +36,38 @@ export default function List({ todoData, setTodoData }) {
   };
 
   // getStyle 함수 (체크박스 누르면 줄 긋도록 동적으로 만들어야함)
-  const getStyle = (completed) => {
-    return {
-      padding: "10px",
-      borderBottom: "1px #ccc dotted",
-      textDecoration: completed ? "line-through" : "none",
-    };
-  };
+  // const getStyle = (completed) => {
+  //   return {
+  //     padding: "10px",
+  //     borderBottom: "1px #ccc dotted",
+  //     textDecoration: completed ? "line-through" : "none",
+  //   };
+  // };
 
   return (
     <div>
       {todoData.map((data) => (
-        <div style={getStyle(data.completed)} key={data.id}>
-          <input
-            type="checkbox"
-            defaultChecked={false}
-            onChange={() => handleCompleChange(data.id)}
-          />
-          {data.title}
-          <button style={btnstyle} onClick={() => handleClick(data.id)}>
-            X
-          </button>
+        <div key={data.id}>
+          <div className="flex items-center justify-between h-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded ">
+            <div className="items-center ">
+              <input
+                type="checkbox"
+                defaultChecked={data.completed}
+                onChange={() => handleCompleChange(data.id)}
+              />{" "}
+              <span className={data.completed ? "line-through" : undefined}>
+                {data.title}{" "}
+              </span>
+            </div>
+            <div className="items-center">
+              <button
+                className="px-4 py-2"
+                onClick={() => handleClick(data.id)}
+              >
+                X
+              </button>
+            </div>
+          </div>
         </div>
       ))}
     </div>
